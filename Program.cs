c154db21e4a3b7ae8e3241486dc6ed1d3faeeee9
DESKTOP-LN7SD4G\user <user>
2025-11-26 19:38:49 -0300

    Teste

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int distribuidos, qtPicaPau, qtRaposinha;
+
+
+Console.WriteLine("--- Esperto Contra Sabido ---\n");
+
+Console.Write("Quantos alimentos serão distribuídos? ");
+distribuidos = Convert.ToInt32(Console.ReadLine()!);
+//6
+
+int class DistribuindoPicaPau
+{
+    
+    
+}
+
